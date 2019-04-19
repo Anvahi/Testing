05088c40f0299c176f5cca0a7a4e5364d4e5ebe2
KrollAlx <Aleksiy2000@yandex.ru>
2019-04-19 17:57:24 +0300

    Normal forms

---
+++ Окно практика 2.0/Окно практика/EditingQuestion.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Окно_практика
+{
+    public partial class EditingQuestion : Form
+    {
+        public EditingQuestion()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void BackToMenu_Click(object sender, EventArgs e)
+        {
+            Hide();
+            TeacherMenu newTeacherMenu = new TeacherMenu();
+            newTeacherMenu.Show();
+        }
+
+        private void EditingQuestion_FormClosed(object sender, FormClosedEventArgs e)
+        {
+            Application.Exit();
+        }
+    }
+}
---
+++ Окно практика 2.0/Окно практика/Form1.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Xml;
+
+namespace Окно_практика
+{
+    public partial class MainForm : Form
+    {
+        bool log = false, pass = false;
+        public MainForm()
+        {
+            InitializeComponent();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            XmlDocument xDoc = new XmlDocument();
+            xDoc.Load("users.xml");
+            //XmlDocument xText = new XmlDocument();
+            //xText.Load("1.xml");
+            XmlElement xRoot = xDoc.DocumentElement;
+            foreach (XmlNode xnode in xRoot)
+            {
+                if (xnode.Name == "teachers")
+                {
+                    foreach (XmlNode childteacher in xnode.ChildNodes)
+                    {
+                        log = false;
+                        pass = false;
+                        foreach (XmlNode childlogpass in childteacher.ChildNodes)
+                        {
+
+                            if (childlogpass.Name == "login" && childlogpass.InnerText == Login.Text)
+                                log = true;
+                            else if (childlogpass.Name == "password" && childlogpass.InnerText == Parol.Text)
+                                pass = true;
+                            if (log && pass)
+                            {
+                                Hide();
+                                TeacherMenu newTeacherMenu = new TeacherMenu();
+                                newTeacherMenu.Show();
+                            }
+                        }
+                    }
+                }
+                else if (xnode.Name == "students")
+                {
+                    foreach (XmlNode childstudents in xnode.ChildNodes)
+                    {
+                        log = false;
+                        pass = false;
+
+                        foreach (XmlNode childlogpass in childstudents.ChildNodes)
+                        {
+
+                            if (childlogpass.Name == "login" && childlogpass.InnerText == Login.Text)
+                                log = true;
+                            else if (childlogpass.Name == "password" && childlogpass.InnerText == Parol.Text)
+                                pass = true;
+                            if (log && pass)
+                            {
+                                Hide();
+                                Меню_для_студента newForm = new Меню_для_студента();
+                                newForm.Show();
+                            }
+                        }
+                    }
+                }
+
+            }
+        }
+
+        private void MainForm_FormClosed(object sender, FormClosedEventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void MainForm_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
