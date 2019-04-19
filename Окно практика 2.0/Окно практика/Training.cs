f15c4963beedf44edfebbf5869fa0ac93824edd7
Michael Sharko <michael.sharko@yandex.ru>
2019-04-19 23:36:18 +0300

    Update Training.cs

--- Окно практика 2.0/Окно практика/Training.cs
+++ Окно практика 2.0/Окно практика/Training.cs
@@ -18,202 +18,202 @@ namespace Окно_практика
             InitializeComponent();
         }
 
-		const int MAX_QUESTIONS = 240;
-		const int MAX_THEMES = 8;
-		const int MAX_THEME_QUESTIONS = 30;
+	const int MAX_QUESTIONS = 240;
+	const int MAX_THEMES = 8;
+	const int MAX_THEME_QUESTIONS = 30;
 
-		private static int amount = 0;
-		private static readonly bool[] isNotOpened = new bool[MAX_QUESTIONS];
+	private static int amount = 0;
+	private static readonly bool[] isNotOpened = new bool[MAX_QUESTIONS];
 
-		private static int cur_theme = 0;
-		private static string cur_answer;
-		private static string cur_question;
-		private static bool cur_isAnswered = true;
-		private static int answered = -1;
+	private static int cur_theme = 0;
+	private static string cur_answer;
+	private static string cur_question;
+	private static bool cur_isAnswered = true;
+	private static int answered = -1; 
 
-		// get (Number of theme - 1)
-		// return Number of question
-		// or -1 if question list is empty
-		private static int GetAnswerNumber(int theme)
+	// get (Number of theme - 1)
+	// return Number of question
+	// or -1 if question list is empty
+	private static int GetAnswerNumber(int theme)
+	{
+
+		if (amount == 0)
 		{
-
-			if (amount == 0)
+			for (int i = 0; i < MAX_QUESTIONS; i++)
 			{
-				for (int i = 0; i < MAX_QUESTIONS; i++)
-				{
-					isNotOpened[i] = true;
-				}
+				isNotOpened[i] = true;
 			}
-
-			if (theme == -1)
-			{
-				return -1;
-			}
-
-			if (amount == MAX_QUESTIONS)
-			{
-				Console.WriteLine("No questions\n");
-				return -1;
-			}
-
-			Random rnd = new Random();
-			int question = theme * MAX_THEME_QUESTIONS + rnd.Next() % MAX_THEME_QUESTIONS;
-			while (!isNotOpened[question])
-			{
-				question = theme * MAX_THEME_QUESTIONS + rnd.Next() % MAX_THEME_QUESTIONS;
-			}
-
-			amount++;
-			isNotOpened[question] = false;
-
-			return question;
 		}
-		private static void UploadQuestionData(int question)
-		{
-			XmlDocument doc = new XmlDocument();
-			doc.Load("resources/questions.xml");
 
-			XmlElement root = doc.DocumentElement;
-			foreach (XmlNode nodes in root)
+		if (theme == -1)
+		{
+			return -1;
+		}
+
+		if (amount == MAX_QUESTIONS)
+		{
+			Console.WriteLine("No questions\n");
+			return -1;
+		}
+
+		Random rnd = new Random();
+		int question = theme * MAX_THEME_QUESTIONS + rnd.Next() % MAX_THEME_QUESTIONS;
+		while (!isNotOpened[question])
+		{
+			question = theme * MAX_THEME_QUESTIONS + rnd.Next() % MAX_THEME_QUESTIONS;
+		}
+
+		amount++;
+		isNotOpened[question] = false;
+
+		return question;
+	}
+	private static void UploadQuestionData(int question)
+	{
+		XmlDocument doc = new XmlDocument();
+		doc.Load("resources/questions.xml");
+
+		XmlElement root = doc.DocumentElement;
+		foreach (XmlNode nodes in root)
+		{
+			foreach (XmlNode cycleNode in nodes)
 			{
-				foreach (XmlNode cycleNode in nodes)
+				if (cycleNode.Attributes.Count > 0)
 				{
-					if (cycleNode.Attributes.Count > 0)
+					XmlNode attr = cycleNode.Attributes.GetNamedItem("id");
+					if (attr.Value == question.ToString())
 					{
-						XmlNode attr = cycleNode.Attributes.GetNamedItem("id");
-						if (attr.Value == question.ToString())
+						foreach (XmlNode quest in cycleNode.ChildNodes)
 						{
-							foreach (XmlNode quest in cycleNode.ChildNodes)
+							if (quest.Name == "quest")
 							{
-								if (quest.Name == "quest")
-								{
-									cur_question = quest.InnerText;
-								}
-								if (quest.Name == "true")
-								{
-									cur_answer = quest.InnerText;
-								}
+								cur_question = quest.InnerText;
+							}
+							if (quest.Name == "true")
+							{
+								cur_answer = quest.InnerText;
 							}
 						}
 					}
 				}
 			}
 		}
+	}
 
-		private void NextQuestion()
+	private void NextQuestion()
+	{
+		if (answered >= 10 - 1)
 		{
-			if (answered >= 10 - 1)
-			{
-				return;
-			}
-
-			answered++;
-
-			UploadQuestionData(GetAnswerNumber(cur_theme - 1));
-			if (cur_theme != 0 && (cur_question != null && cur_isAnswered == true))
-			{
-				Question.Text = cur_question;
-				cur_isAnswered = false;
-			}
+			return;
 		}
 
-		private void button1_Click(object sender, EventArgs e)
-        {
-            /*XmlDocument xTraining = new XmlDocument();
-            xTraining.Load("testing.xml");*/
+		answered++;
 
-            if (Cycles.Checked==true)
-            {
+		UploadQuestionData(GetAnswerNumber(cur_theme - 1));
+		if (cur_theme != 0 && (cur_question != null && cur_isAnswered == true))
+		{
+			Question.Text = cur_question;
+			cur_isAnswered = false;
+		}
+	}
+
+	private void button1_Click(object sender, EventArgs e)
+	{
+	    /*XmlDocument xTraining = new XmlDocument();
+	    xTraining.Load("testing.xml");*/
+
+	    if (Cycles.Checked==true)
+	    {
 				cur_theme = 1;
 			}
-            if (Arrays.Checked == true)
-			{
-				cur_theme = 2;
-			}
-			if (Strings.Checked == true)
-			{
-				cur_theme = 3;
-			}
-			if (Recursion.Checked == true)
-			{
-				cur_theme = 4;
-			}
-			if (Structs.Checked == true)
-			{
-				cur_theme = 5;
-			}
-			if (Files.Checked == true)
-			{
-				cur_theme = 6;
-			}
-			if (Pointers.Checked == true)
-			{
-				cur_theme = 7;
-			}
-			if (Dynamic.Checked == true)
-			{
-				cur_theme = 8;
-			}
+	    if (Arrays.Checked == true)
+		{
+			cur_theme = 2;
+		}
+		if (Strings.Checked == true)
+		{
+			cur_theme = 3;
+		}
+		if (Recursion.Checked == true)
+		{
+			cur_theme = 4;
+		}
+		if (Structs.Checked == true)
+		{
+			cur_theme = 5;
+		}
+		if (Files.Checked == true)
+		{
+			cur_theme = 6;
+		}
+		if (Pointers.Checked == true)
+		{
+			cur_theme = 7;
+		}
+		if (Dynamic.Checked == true)
+		{
+			cur_theme = 8;
+		}
 
+		NextQuestion();
+	}
+
+	private void button3_Click(object sender, EventArgs e)
+	{
+	    Hide();
+	    Меню_для_студента newForm = new Меню_для_студента();
+	    newForm.Show(); 
+	}
+
+	private void Training_FormClosed(object sender, FormClosedEventArgs e)
+	{
+	    Application.Exit();
+	}
+
+	private void label1_Click(object sender, EventArgs e)
+	{
+
+	}
+
+	private void button2_Click(object sender, EventArgs e)
+	{
+		if (cur_answer == null)
+		{
+			return;
+		}
+
+		switch (cur_answer[0])
+		{
+		case '1':
+			if (Answer1.Checked == true)
+			{
+				cur_isAnswered = true;
+			}
+			break;
+		case '2':
+			if (Answer2.Checked == true)
+			{
+				cur_isAnswered = true;
+			}
+			break;
+		case '3':
+			if (Answer3.Checked == true)
+			{
+				cur_isAnswered = true;
+			}
+			break;
+		case '4':
+			if (Answer4.Checked == true)
+			{
+				cur_isAnswered = true;
+			}
+			break;			
+		}
+
+		if (cur_isAnswered)
+		{
 			NextQuestion();
 		}
-
-        private void button3_Click(object sender, EventArgs e)
-        {
-            Hide();
-            Меню_для_студента newForm = new Меню_для_студента();
-            newForm.Show(); 
-        }
-
-        private void Training_FormClosed(object sender, FormClosedEventArgs e)
-        {
-            Application.Exit();
-        }
-
-        private void label1_Click(object sender, EventArgs e)
-        {
-
-        }
-
-        private void button2_Click(object sender, EventArgs e)
-        {
-			if (cur_answer == null)
-			{
-				return;
-			}
-
-			switch (cur_answer[0])
-			{
-				case '1':
-					if (Answer1.Checked == true)
-					{
-						cur_isAnswered = true;
-					}
-					break;
-				case '2':
-					if (Answer2.Checked == true)
-					{
-						cur_isAnswered = true;
-					}
-					break;
-				case '3':
-					if (Answer3.Checked == true)
-					{
-						cur_isAnswered = true;
-					}
-					break;
-				case '4':
-					if (Answer4.Checked == true)
-					{
-						cur_isAnswered = true;
-					}
-					break;			
-			}
-
-			if (cur_isAnswered)
-			{
-				NextQuestion();
-			}
-		}
+	}
     }
 }
